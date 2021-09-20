c7ee0d9d053f2096e86f936b0a6f3e79acf26d5c
MrSkyGold <devmarcos.py@gmail.com>
2021-09-20 00:08:41 -0300

    Adicionado relação cliente em serviços

--- servicos/models.py
+++ servicos/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from tecnicos.models import Tecnico
 from produtos.models import Produto
-from accounts.models import User
+from accounts.models import Cliente
 
 
 # Create your models here.
@@ -21,8 +21,8 @@ class Servico(models.Model):
     tipo_servico = models.CharField(choices=TIPO_SERVICO_CHOICES, verbose_name='Tipo de Serviço', max_length=25)
     data_servico = models.DateField(auto_now=True, verbose_name='Data de Inicio')
     status = models.CharField(choices=STATUS_CHOICES, max_length=25)
-    tecnico = models.ForeignKey(Tecnico, related_name='tecnico',on_delete=models.DO_NOTHING)
-    cliente = models.ForeignKey(User, related_name='user', on_delete=models.DO_NOTHING)
+    tecnico = models.ForeignKey(Tecnico, related_name='tecnico', on_delete=models.DO_NOTHING)
+    cliente = models.ForeignKey(Cliente, related_name='cliente', on_delete=models.DO_NOTHING)
     produto = models.ForeignKey(Produto, related_name='produto', on_delete=models.DO_NOTHING)
 
     def __str__(self):
