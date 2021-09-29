01fe17e4bf76260c544a3b1a9f44cf9b957d0375
MrSkyGold <devmarcos.py@gmail.com>
2021-09-29 10:18:05 -0300

    settings trocado db linha 87 e 91

--- helpdesk/settings.py
+++ helpdesk/settings.py
@@ -84,11 +84,11 @@ WSGI_APPLICATION = 'helpdesk.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'sqlite': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     },
-    "default1": {
+    "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "postgres",
         "USER": "postgres",
